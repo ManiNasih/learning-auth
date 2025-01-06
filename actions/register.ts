446415034642b7a80241ca82a20f40931be9cf29
ManiNasih <w5824805@gmail.com>
2025-01-06 16:16:41 +0300

    add postgres db with prisma orm and add create use functionlity

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,10 @@
+"use server";
+
 import { registerSchema } from "@/schemas";
 import { z } from "zod";
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validatedField = registerSchema.safeParse(values);
@@ -8,5 +13,23 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
     return { error: "Invalidated fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedField.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User created" };
 };
